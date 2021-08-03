bcba78b4b9c4dbbbd40c98d265f05bdd036ef078
Oleg Polyanskiy <ol.polyanskiy@gmail.com>
2021-08-03 10:40:06 +0300

    connect redux and devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './main.global.scss';
 import { hot } from 'react-hot-loader/root';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Layout } from './shared/Layout';
 import { Header } from './shared/Header';
 import { Content } from './shared/Content';
@@ -8,28 +11,32 @@ import { CardsList } from './shared/Content/CardsList';
 import { useToken } from './hooks/useToken';
 import { commentContext, PostsContextProvider, tokenContext } from './shared/context';
 
+const store = createStore(() => {}, composeWithDevTools());
+
 function AppComponent() {
   const [commentValue, setCommentValue] = useState('');
   const [token] = useToken();
   const CommentProvider = commentContext.Provider;
 
   return (
-    <CommentProvider value={{
-      value: commentValue,
-      onChange: setCommentValue,
-    }}
-    >
-      <tokenContext.Provider value={token}>
-        <PostsContextProvider>
-          <Layout>
-            <Header />
-            <Content>
-              <CardsList />
-            </Content>
-          </Layout>
-        </PostsContextProvider>
-      </tokenContext.Provider>
-    </CommentProvider>
+    <Provider store={store}>
+      <CommentProvider value={{
+        value: commentValue,
+        onChange: setCommentValue,
+      }}
+      >
+        <tokenContext.Provider value={token}>
+          <PostsContextProvider>
+            <Layout>
+              <Header />
+              <Content>
+                <CardsList />
+              </Content>
+            </Layout>
+          </PostsContextProvider>
+        </tokenContext.Provider>
+      </CommentProvider>
+    </Provider>
   );
 }
 
